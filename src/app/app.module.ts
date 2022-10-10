ec9115ad31758257d4f098843c3c9c5f4a3fa92e
David Ruen <davidruen@gmail.com>
2022-10-10 20:51:32 +0200

    update avancee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ContainerComponent } from './component/container/container.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ProductComponent } from './component/product/product.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
+import { ProductItemComponent } from './component/product-item/product-item.component';
+import { ModalProductViewComponent } from './component/modal-product-view/modal-product-view.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent,
+    FooterComponent,
+    ProductComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ModalProductViewComponent,
+   
   ],
   imports: [
     BrowserModule
